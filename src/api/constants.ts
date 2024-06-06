b6a8d389a2712de4f40a8fe1bc05dd240a93f8e2
Martin Emanuel <martinemanuel93@gmail.com>
2024-06-06 14:35:12 -0300

    Fix public env

--- src/api/constants.ts
+++ src/api/constants.ts
@@ -1,5 +1,5 @@
 export  const ARTICLES_URL = import.meta.env.DEV
   ? "/data.json"
-  : import.meta.env.ARTICLES_URL;
+  : import.meta.env.VITE_ARTICLES_URL;
 
 export const BASE_URL_DUMMY = "/data.json";
